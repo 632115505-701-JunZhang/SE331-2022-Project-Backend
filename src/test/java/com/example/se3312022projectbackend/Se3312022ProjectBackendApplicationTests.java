db6533766ed431280d5f604dc129ff230880a0e6
JIANGTAO <jiang_tao@cmu.ac.th>
2022-10-29 16:11:45 +0800

    InitialCommit(CreatProject)

---
+++ src/test/java/com/example/se3312022projectbackend/Se3312022ProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.se3312022projectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se3312022ProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
